cec2c8427514de717c493fb868e7f23c010c48d3
windx-foobar <bezalkogoln1ycoder@gmail.com>
2022-05-12 00:43:51 +0300

    rdy model to example (in progress)

---
+++ app/models/User.ts
@@ -0,0 +1,73 @@
+import {
+  Table,
+  Column,
+  DataType,
+  BeforeCreate,
+  BeforeUpdate,
+  DefaultScope
+} from 'sequelize-typescript';
+import { Model } from '@packages/advanced-sequelize';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import * as bcrypt from 'bcrypt';
+
+@DefaultScope(() => (
+  {
+    attributes: {
+      exclude: ['password']
+    }
+  }
+))
+@Table({ tableName: 'users', underscored: true, paranoid: true })
+export class User extends Model {
+  @BeforeCreate
+  public static async hashBeforeCreate(model: User) {
+    if (model.password === 'hashedPassword' && model.hashedPassword) {
+      model.password = model.hashedPassword;
+    } else {
+      await model.hashPassword();
+    }
+  }
+
+  @BeforeUpdate
+  public static async hashBeforeUpdate(model: User) {
+    if (!model.changed('password')) return;
+    await this.hashBeforeCreate(model);
+  }
+
+  @IsNotEmpty({ message: 'Поле обязательно для заполнения' })
+  @IsEmail(undefined, { message: 'Поле должно быть корректным email' })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+    unique: { name: 'uniqueEmail', msg: 'E-mail уже используется' }
+  })
+  public email: string;
+
+  @IsNotEmpty({ message: 'Поле обязательно для заполнения' })
+  @MinLength(5, { message: 'Поле должно быть не менее $constraint1 символов' })
+  @Column({ type: DataType.STRING, allowNull: false })
+  public password: string;
+
+  @Column({
+    type: DataType.VIRTUAL,
+    set(val: string) {
+      this.setDataValue('hashedPassword', val);
+    }
+  })
+  public hashedPassword: string;
+
+  @Column({ type: DataType.JSONB, defaultValue: {} })
+  public payload: object;
+
+  public async hashPassword() {
+    if (!this.changed('password')) return;
+    try {
+      const { password } = this;
+      const salt = await bcrypt.genSalt();
+
+      this.password = await bcrypt.hash(password, salt);
+    } catch (error) {
+      throw error;
+    }
+  }
+}
