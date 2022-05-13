83924da6b7bc6fbe099c2e80c9de4cbf6c337560
windx-foobar <bezalkogoln1ycoder@gmail.com>
2022-05-14 00:31:15 +0300

    *

--- packages/core/utils/index.ts
+++ packages/core/utils/index.ts
@@ -2,8 +2,8 @@ import { glob as _glob } from 'glob';
 import { exec as _exec, spawn as _spawn } from 'child_process';
 import { promisify } from 'util';
 
+export type Maybe<T> = T | undefined | null;
+
 export const glob = promisify(_glob);
 export const exec = promisify(_exec);
 export const spawn = promisify(_spawn);
-
-export type Maybe<T> = T | undefined | null;
