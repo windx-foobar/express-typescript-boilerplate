a6de52eb7fa36ea3f442e663cc895cf49e60e981
windx-foobar <bezalkogoln1ycoder@gmail.com>
2022-05-14 00:30:46 +0300

    env -> config

--- packages/core/loaders/homeLoader.ts
+++ packages/core/loaders/homeLoader.ts
@@ -1,7 +1,6 @@
 import * as express from 'express';
 import { MicroframeworkLoader, MicroframeworkSettings } from 'microframework-w3tec';
-
-import { env } from '../env';
+import { config } from '../config';
 
 export const homeLoader: MicroframeworkLoader = (
   settings: MicroframeworkSettings | undefined
@@ -9,12 +8,12 @@ export const homeLoader: MicroframeworkLoader = (
   if (settings) {
     const expressApp = settings.getData('express_app');
     expressApp.get(
-      env.app.routePrefix,
+      config.app.routePrefix,
       (req: express.Request, res: express.Response) => {
         return res.json({
-          name: env.app.name,
-          version: env.app.version,
-          description: env.app.description
+          name: config.app.name,
+          version: config.app.version,
+          description: config.app.description
         });
       }
     );
--- packages/core/loaders/sequelizeLoader.ts
+++ packages/core/loaders/sequelizeLoader.ts
@@ -1,47 +1,28 @@
 import { MicroframeworkLoader, MicroframeworkSettings } from 'microframework-w3tec';
 import { SequelizeOptions, Sequelize } from 'sequelize-typescript';
-// import { glob } from '@packages/core/utils';
 import { Container } from 'typedi';
-
-import { env } from '../env';
+import { config } from '../config';
 
 export const sequelizeLoader: MicroframeworkLoader = async (
   settings: MicroframeworkSettings | undefined
 ): Promise<void> => {
   const connectionOptions: SequelizeOptions = {
-    dialect: env.db.type as any,
-    host: env.db.host,
-    port: env.db.port,
-    username: env.db.username,
-    password: env.db.password,
-    database: env.db.database,
+    dialect: config.db.type as any,
+    host: config.db.host,
+    port: config.db.port,
+    username: config.db.username,
+    password: config.db.password,
+    database: config.db.database,
     pool: {
       min: 0,
       max: 5,
       idle: 10000
     },
-    models: [env.app.dirs.entitiesDir]
+    logging: config.isProduction ? false : console.log,
+    models: [config.app.dirs.entitiesDir]
   };
 
   const connection = new Sequelize(connectionOptions);
-
-  /*await Promise.all(
-   env.app.dirs.entities.map(async (path: string) => {
-   const modelPaths = await glob(path);
-
-   modelPaths.forEach((modelPath: string) => {
-   require(modelPath).default.initialize({
-   sequelize: connection
-   });
-   });
-   })
-   );
-
-   Object.keys(connection.models)
-   .forEach((modelName: string) => {
-   const model = connection.models[modelName] as any;
-   model.association(connection.models);
-   });*/
 
   try {
     await connection.authenticate();
