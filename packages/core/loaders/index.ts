cbcfce8e1bdbc26a2a9d25edabcca7ad45223750
windx-foobar <bezalkogoln1ycoder@gmail.com>
2022-05-14 00:20:28 +0300

    файл экспорта лоадеров

---
+++ packages/core/loaders/index.ts
@@ -0,0 +1,6 @@
+export * from './expressLoader';
+export * from './homeLoader';
+export * from './iocLoader';
+export * from './publicLoader';
+export * from './sequelizeLoader';
+export * from './winstonLoader';
