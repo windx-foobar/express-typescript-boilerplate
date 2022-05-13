d94e143b5190f45531958031dae4982b555551e2
windx-foobar <bezalkogoln1ycoder@gmail.com>
2022-05-14 00:30:17 +0300

    добавил логгирование в файлы для прода

--- packages/core/loaders/winstonLoader.ts
+++ packages/core/loaders/winstonLoader.ts
@@ -1,25 +1,49 @@
 import { MicroframeworkLoader, MicroframeworkSettings } from 'microframework-w3tec';
-import { configure, format, transports } from 'winston';
-
-import { env } from '../env';
+import * as winston from 'winston';
+import 'winston-daily-rotate-file';
+import { config } from '../config';
+import path from 'path';
 
 export const winstonLoader: MicroframeworkLoader = (
   settings: MicroframeworkSettings | undefined
 ): void => {
-  configure({
-    transports: [
-      new transports.Console({
-        level: env.log.level,
-        handleExceptions: true,
-        format: env.node !== 'development'
-          ? format.combine(
-            format.json()
-          )
-          : format.combine(
-            format.colorize(),
-            format.simple()
-          )
+  const transports: any[] = [];
+
+  if (config.isProduction) {
+    transports.push(
+      new winston.transports.DailyRotateFile({
+        level: config.log.level,
+        filename: 'production-log',
+        extension: '.log',
+        dirname: path.resolve(process.cwd(), 'logs'),
+        datePattern: 'YYYY-MM-DD',
+        maxSize: '2m',
+        format: winston.format.combine(
+          winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+          winston.format.printf(({ level, message, timestamp }) => {
+            try {
+              const parsed = JSON.parse(message);
+
+              return `[${timestamp}][${level}] ${parsed}`;
+            } catch (error) {
+              return `[${timestamp}][${level}] ${message}`;
+            }
+          })
+        ),
+        maxFiles: '7d'
       })
-    ]
-  });
+    );
+  } else {
+    transports.push(
+      new winston.transports.Console({
+        level: config.log.level,
+        handleExceptions: true,
+        format: config.node !== 'development'
+          ? winston.format.combine(winston.format.json())
+          : winston.format.combine(winston.format.colorize(), winston.format.simple())
+      })
+    );
+  }
+
+  winston.configure({ transports });
 };
