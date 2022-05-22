d0de1d7e3b4c84242433fb9bb0ca7f21c487ce27
windx-foobar <bezalkogoln1ycoder@gmail.com>
2022-05-22 23:06:40 +0300

    добавил utility type AnyFunction

--- packages/core/utils/index.ts
+++ packages/core/utils/index.ts
@@ -3,6 +3,7 @@ import { exec as _exec, spawn as _spawn } from 'child_process';
 import { promisify } from 'util';
 
 export type Maybe<T> = T | undefined | null;
+export type AnyFunction = (...args: any[]) => any;
 
 export const glob = promisify(_glob);
 export const exec = promisify(_exec);
